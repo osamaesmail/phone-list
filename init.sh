38c8f24e24529e996840594b7fd03bd4b567f70a
Osama Esmail <osamaesmail2@gmail.com>
2021-11-07 11:14:41 +0200

    update init

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ git submodule update --init --recursive --remote --merge
 
 cp phone-list-be/.env.example .env.be
 cp phone-list-fe/.env.example .env.fe
+cp phone-list-fe/.env.example phone-list-fe/.env
